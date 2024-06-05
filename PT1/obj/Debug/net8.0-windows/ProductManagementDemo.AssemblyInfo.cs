3df99020e184e0adca0beb0ca1017ee4a9badd84
Vu Nguyen <vunguyen2300@gmail.com>
2024-06-05 13:27:30 +0700

    lab01

--- PT1/obj/Debug/net8.0-windows/ProductManagementDemo.AssemblyInfo.cs
+++ PT1/obj/Debug/net8.0-windows/ProductManagementDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagementDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f368b381a2e4e080d9cf566923b5ac1604f50d48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf160d0078af0a1363d5c5619553d527b446a75")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagementDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagementDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
